6896ff29011a54b55527f1d55b525c00f8c9ccd1
Sarah Clark <seclark@nextquestion.net>
2024-12-19 11:44:35 -0800

    Add daisyui & typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,17 @@
 import { type Config } from "tailwindcss";
+import daisyui from "daisyui"
+import typography from "@tailwindcss/typography";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
+  plugins: [
+    typography,
+    // @ts-expect-error typical type error w/ plugins
+    daisyui
+  ],
+  daisyui: {
+    themes: [ "nord" ]
+  },
 } satisfies Config;
